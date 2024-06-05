e51d5ce864cf072c66f072afbea32a574e01747b
Tobias Schmid <tobias.schmid@fau.de>
2024-06-05 10:34:58 +0200

    final version of data report

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -3,3 +3,4 @@ rm ./data/population.sqlite
 jv -d ./project/pipeline.jv
 python3 ./project/pipeline2.py
 python3 ./project/plot.py
+# pandoc ./project/data-report.md -o ./project/data-report.pdf -V papersize:a4 -V geometry:margin=2.5cm -V fontsize=11pt
