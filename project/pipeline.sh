0cfe691d732ff3d04fdacd70ca2c386718fc0705
Tobias Schmid <tobias.schmid@fau.de>
2024-06-25 12:19:30 +0200

    test sqlite databas

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm ./data/population.sqlite
 jv -d ./project/pipeline.jv
+cd data
+ls 
+cat population.sqlite
+cd ..
 python3 ./project/pipeline2.py
 # python3 ./project/plot.py
 # pandoc ./project/data-report.md -o ./project/data-report.pdf -V papersize:a4 -V geometry:margin=2.5cm -V fontsize=11pt
